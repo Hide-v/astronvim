b956ac4c4cfc35bee26247d109f87e1b468962d2
Hide-v <vcmingri19@outlook.com>
2026-07-08 11:30:13 +0800

    feat(mappings): 添加多模式行移动映射

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -7,11 +7,19 @@ return {
         -- first key is the mode
         i = {
           ["<C-S-v>"] = { "<C-r>+", desc = "Paste from system clipboard" },
+          ["<M-Up>"] = { "<Esc>:m -2<CR>gi", desc = "Move line up" },
+          ["<M-Down>"] = { "<Esc>:m +2<CR>gi", desc = "Move line down" },
         },
         n = {
           ["<C-/>"] = { "<Cmd>ToggleTerm<CR>", desc = "Toggle Terminal" },
           ["<C-d>"] = { "10jzz" },
           ["<C-u>"] = { "10kzz" },
+          ["<M-Up>"] = { ":m -2<CR>", desc = "Move line up" },
+          ["<M-Down>"] = { "<Esc>:m +2<CR>gi", desc = "Move line down" },
+        },
+        v = {
+          ["<M-Up>"] = { ":m '<-2<CR>gv", desc = "Move selected lines up" },
+          ["<M-Down>"] = { "<Esc>:m +2<CR>gi", desc = "Move line down" },
         },
         t = {
           ["<C-/>"] = { "<Cmd>ToggleTerm<CR>", desc = "Toggle Terminal" },
