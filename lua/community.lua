109c782dfc226c2712d0da77715f35b73e2a160d
Hide-v <vcmingri19@outlook.com>
2026-08-04 22:05:38 +0800

    feat: 添加 JSON/Vue 语言支持和透明主题

--- lua/community.lua
+++ lua/community.lua
@@ -11,7 +11,10 @@ return {
   { import = "astrocommunity.recipes.vscode" },
   { import = "astrocommunity.editing-support.auto-save-nvim" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.code-runner.overseer-nvim" },
+  { import = "astrocommunity.color.transparent-nvim" },
   -- import/override with your plugins folder
 }
