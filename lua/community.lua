6c020191bd01aaccdfc5164cefc262ffde9d79d0
Hide-v <vcmingri19@outlook.com>
2026-08-27 15:35:48 +0800

    feat: 添加C#语言支持

--- lua/community.lua
+++ lua/community.lua
@@ -11,6 +11,7 @@ return {
   { import = "astrocommunity.recipes.vscode" },
   { import = "astrocommunity.editing-support.auto-save-nvim" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.cs" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.pack.markdown" },
