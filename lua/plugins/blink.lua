f301c5b4c6bf627893612b05629ae3b8ca116ebc
Hide-v <vcmingri19@outlook.com>
2026-09-20 12:17:00 +0800

    fix(blink): 禁用补全文档自动显示

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -42,7 +42,7 @@ return {
           },
         },
         documentation = {
-          auto_show = true,
+          auto_show = false,
           auto_show_delay_ms = 2000,
         },
         menu = {
